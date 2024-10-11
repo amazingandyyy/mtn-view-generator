a6734e48dc7fafa5f9a828303b6ba926009f2fee
Andy Chen <amazingandyyy@gmail.com>
2024-10-11 19:45:19 +0800

    feat: update path

--- docs/install.sh
+++ docs/install.sh
@@ -1,5 +1,5 @@
 pushd ~/Desktop/mtn-view
-curl -sL https://amazingandyyy.com/mtn-view-generator/index.js > ~/Desktop/mtn-view/index.js
+curl -sL https://raw.githubusercontent.com/amazingandyyy/mtn-view-generator/refs/heads/main/docs/index.js > ~/Desktop/mtn-view/index.js
 rm -rf $(which mtn-view) && npm i -g . --force
 which mtn-view
 echo "前往: https://amazingandyyy.com/mtn-view-generator"
